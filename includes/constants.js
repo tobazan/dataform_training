050fda7e8408acc40f20aae2a2fb72e4c8b95650
tobazan <bazanme90@gmail.com>
2023-01-17 17:47:30 -0300

    change endDate constant to analyze larger timeframe

--- includes/constants.js
+++ includes/constants.js
@@ -1,5 +1,5 @@
 const startDate = "20170101";
-const endDate = "20170103";
+const endDate = "20170115";
 const test_table = "`bigquery-public-data.google_analytics_sample.ga_sessions_*`"
 
 module.exports = {startDate, endDate, test_table}
