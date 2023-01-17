b039fa1a561f3ba3dde4f56da87e892bc179e51a
Matias Bazan <matias.bazan@mediamonks.com>
2023-01-17 15:50:03 +0000

    fix constant pointing to BigQuery table

--- includes/constants.js
+++ includes/constants.js
@@ -1,5 +1,5 @@
 const startDate = "20170101";
 const endDate = "20170103";
-const test_table = "ga_sessions_*"
+const test_table = "bigquery-public-data.google_analytics_sample.ga_sessions_*"
 
 module.exports = {startDate, endDate}
